7a89d94300434d52bae520be6d7b1bddeb81a6f0
LachieWestlake <lachlanwestlake@gmail.com>
2021-10-06 17:28:11 +1100

    Added ability to see what you changed the date to

--- src/components/sensorDataTab/DatePicker.tsx
+++ src/components/sensorDataTab/DatePicker.tsx
@@ -1,6 +1,8 @@
-import React, { useState } from 'react'
-import { Button, View } from 'react-native'
+import React, { useRef, useState } from 'react'
+import { Button, View, TextInput } from 'react-native'
 import DateTimePickerModal from 'react-native-modal-datetime-picker'
+import { Text, useTheme } from 'react-native-elements'
+import { format } from 'date-fns'
 
 const DatePicker = () => {
   const [isDatePickerVisible, setDatePickerVisibility] = useState(false)
@@ -15,18 +17,30 @@ const DatePicker = () => {
 
   const handleConfirm = (date: any) => {
     console.warn('A date has been picked: ', date)
+    // let inputs = [date];
+    // inputs.push(<TextInput style={{color:theme.colors?.white }}editable={false} value={componentRef.current.toString()}></TextInput>);
+    componentRef.current = format(date, 'dd-MM-yyyy')
     hideDatePicker()
   }
 
+  const componentRef = useRef('')
+
+  const { theme } = useTheme()
+
   return (
     <View>
-      <Button title="Show Date Picker" onPress={showDatePicker} />
+      <Button title="Change Date" onPress={showDatePicker} />
       <DateTimePickerModal
         isVisible={isDatePickerVisible}
         mode="date"
         onConfirm={handleConfirm}
         onCancel={hideDatePicker}
       />
+      <TextInput
+        style={{ color: theme.colors?.white }}
+        editable={false}
+        value={componentRef.current.toString()}
+      ></TextInput>
     </View>
   )
 }
